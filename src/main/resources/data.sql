db58b20150942e6f7ef54f5fc8c3309e2b3338c6
ivan77d <ivandrokin@gmail.com>
2022-05-05 15:18:39 +0300

    1

---
+++ src/main/resources/data.sql
@@ -0,0 +1,19 @@
+INSERT INTO user VALUES ('1','11','Jjj','1','jjjov','user1');
+INSERT INTO user VALUES ('2','22','Qqq','1','Qqqov','user2');
+INSERT INTO user VALUES ('3','22','Www','1','Wwwov','user3');
+INSERT INTO user VALUES ('4','44','Vvv','1','Vvvov','user4');
+INSERT INTO user VALUES ('5','55','Aaa' ,'2','Aaaov','user5');
+
+INSERT INTO role VALUES ('1', 'ROLE_USER');
+INSERT INTO role VALUES ('2', 'ROLE_ADMIN');
+
+
+INSERT INTO user_role VALUES ('1', '1');
+INSERT INTO user_role VALUES ('2', '1');
+INSERT INTO user_role VALUES ('3', '1');
+INSERT INTO user_role VALUES ('4', '1');
+INSERT INTO user_role VALUES ('5', '2');
+
+
+
+
